288ff7bd83b01f2a544a035369a001feb6f47215
Matt Priour <mpriour@esri.com>
2025-02-19 12:58:23 -0600

    update listen host on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: { port: process.env.NODE_ENV === "production" ? 8080 : 3000 },
-  plugins: [
-    reactRouter(),
-    tsconfigPaths()
-  ]
+  server: { port: process.env.NODE_ENV === "production" ? 8080 : 3000, host: "0.0.0.0" },
+  plugins: [reactRouter(), tsconfigPaths()]
 });
