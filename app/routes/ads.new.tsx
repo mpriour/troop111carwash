c43ccbf2e16e9dede60b131054505bb442e144ef
Matt Priour <mpriour@esri.com>
2026-03-03 10:02:07 -0600

    make step 1 obvious in create ad workflow

--- app/routes/ads.new.tsx
+++ app/routes/ads.new.tsx
@@ -100,144 +100,151 @@ export default function NewAdPage() {
   const actionData = useActionData() as ActionData;
 
   return (
-    <Form
-      method="post"
-      className="flex flex-col gap-2 mx-auto p-6 max-w-2xl"
-    >
-      {!actionData?.imgSrc || actionData?.error?.id === "img_upload" ? (
-        <div>
-          <label className="flex w-full flex-col gap-1">
-            <span>Image to Upload: </span>
-            <input type="file" name="img" id="img_upload" accept="image/*" />
-            <button
-              type="submit"
-              name="action"
-              value="upload"
-              formEncType="multipart/form-data"
-              className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-            >
-              Upload to Cloudinary
-            </button>
-          </label>
-          {actionData?.error?.id == "img_upload" ? <div className="pt-1 text-red-700" id="img_upload-error">
+    <>
+      <h1 className="text-2xl font-bold text-center mb-4">Create a new Ad</h1>
+      <Form
+        method="post"
+        className="flex flex-col gap-2 mx-auto p-6 max-w-2xl"
+      >
+        {!actionData?.imgSrc || actionData?.error?.id === "img_upload" ? (
+          <div>
+            <h2 className="text-lg font-semibold mb-2">Step 1: Upload your image</h2>
+            <label className="flex w-full flex-col gap-1">
+              <span>Image to Upload: </span>
+              <input type="file" name="img" id="img_upload" accept="image/*" />
+              <button
+                type="submit"
+                name="action"
+                value="upload"
+                formEncType="multipart/form-data"
+                className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+              >
+                Upload to Cloudinary
+              </button>
+            </label>
+            {actionData?.error?.id == "img_upload" ? <div className="pt-1 text-red-700" id="img_upload-error">
               {actionData.error.msg}
             </div> : null}
-        </div>
-      ) : (
-        <div>
-          <label className="flex w-full flex-col gap-1">
-            <span>Img URL: </span>
-            <input
-              name="imgUrl"
-              value={actionData.imgSrc}
-              readOnly={true}
-              className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-            />
-          </label>
-          {actionData?.error?.id == "imgUrl" ? (
-            <div className="pt-1 text-red-700" id="imgUrl-error">
-              {actionData.error.msg}
-            </div>
-          ) : (
+          </div>
+        ) : (
+          <div>
+            <label className="flex w-full flex-col gap-1">
+              <span>Img URL: </span>
+              <input
+                name="imgUrl"
+                value={actionData.imgSrc}
+                readOnly={true}
+                className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+              />
+            </label>
+            {actionData?.error?.id == "imgUrl" ? (
+              <div className="pt-1 text-red-700" id="imgUrl-error">
+                {actionData.error.msg}
+              </div>
+            ) : (
+              <div>
+                <img src={actionData.imgSrc} alt="" />
+              </div>
+            )}
+          </div>
+        )}
+        {actionData?.imgSrc && (
+          <>
             <div>
-              <img src={actionData.imgSrc} alt="" />
+              <label className="flex w-full flex-col gap-1">
+                <span>Sponsor: </span>
+                <input
+                  name="sponsor"
+                  className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+                />
+              </label>
+              {actionData?.error?.id == "sponsor" ? (
+                <div className="pt-1 text-red-700" id="sponsor-error">
+                  {actionData.error.msg}
+                </div>
+              ) : null}
             </div>
-          )}
-        </div>
-      )}
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Sponsor: </span>
-          <input
-            name="sponsor"
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-          />
-        </label>
-        {actionData?.error?.id == "sponsor" ? (
-          <div className="pt-1 text-red-700" id="sponsor-error">
-            {actionData.error.msg}
-          </div>
-        ) : null}
-      </div>
 
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Sponsor URL: </span>
-          <input
-            name="sponsorUrl"
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-          />
-        </label>
-      </div>
+            <div>
+              <label className="flex w-full flex-col gap-1">
+                <span>Sponsor URL: </span>
+                <input
+                  name="sponsorUrl"
+                  className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+                />
+              </label>
+            </div>
 
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Year: </span>
-          <input
-            name="year"
-            defaultValue={DEFAULT_YEAR}
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-          />
-        </label>
-        {actionData?.error?.id == "year" ? (
-          <div className="pt-1 text-red-700" id="year-error">
-            {actionData.error.msg}
-          </div>
-        ) : null}
-      </div>
+            <div>
+              <label className="flex w-full flex-col gap-1">
+                <span>Year: </span>
+                <input
+                  name="year"
+                  defaultValue={DEFAULT_YEAR}
+                  className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+                />
+              </label>
+              {actionData?.error?.id == "year" ? (
+                <div className="pt-1 text-red-700" id="year-error">
+                  {actionData.error.msg}
+                </div>
+              ) : null}
+            </div>
 
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Size: </span>
-          <select
-            name="size"
-            defaultValue={""}
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-          >
-            <option value="">Select a size</option>
-            <option value="1">SMALL</option>
-            <option value="2">MEDIUM</option>
-            <option value="3">LARGE</option>
-          </select>
-        </label>
-        {actionData?.error?.id == "size" ? (
-          <div className="pt-1 text-red-700" id="size-error">
-            {actionData.error.msg}
-          </div>
-        ) : null}
-      </div>
+            <div>
+              <label className="flex w-full flex-col gap-1">
+                <span>Size: </span>
+                <select
+                  name="size"
+                  defaultValue={""}
+                  className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+                >
+                  <option value="">Select a size</option>
+                  <option value="1">SMALL</option>
+                  <option value="2">MEDIUM</option>
+                  <option value="3">LARGE</option>
+                </select>
+              </label>
+              {actionData?.error?.id == "size" ? (
+                <div className="pt-1 text-red-700" id="size-error">
+                  {actionData.error.msg}
+                </div>
+              ) : null}
+            </div>
 
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Orientation: </span>
-          <select
-            name="orient"
-            defaultValue={""}
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-          >
-            <option value="">Choose one</option>
-            <option value="l">Landscape</option>
-            <option value="p">Portrait</option>
-          </select>
-        </label>
-        {actionData?.error?.id == "orient" ? (
-          <div className="pt-1 text-red-700" id="orient-error">
-            {actionData.error.msg}
-          </div>
-        ) : null}
-      </div>
+            <div>
+              <label className="flex w-full flex-col gap-1">
+                <span>Orientation: </span>
+                <select
+                  name="orient"
+                  defaultValue={""}
+                  className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+                >
+                  <option value="">Choose one</option>
+                  <option value="l">Landscape</option>
+                  <option value="p">Portrait</option>
+                </select>
+              </label>
+              {actionData?.error?.id == "orient" ? (
+                <div className="pt-1 text-red-700" id="orient-error">
+                  {actionData.error.msg}
+                </div>
+              ) : null}
+            </div>
 
-      <div className="text-right">
-        <button
-          type="submit"
-          name="action"
-          value="create"
-          formEncType="application/x-www-form-urlencoded"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-        >
-          Save
-        </button>
-      </div>
-    </Form>
+            <div className="text-right">
+              <button
+                type="submit"
+                name="action"
+                value="create"
+                formEncType="application/x-www-form-urlencoded"
+                className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+              >
+                Save
+              </button>
+            </div>
+          </>)}
+      </Form>
+    </>
   );
 }
